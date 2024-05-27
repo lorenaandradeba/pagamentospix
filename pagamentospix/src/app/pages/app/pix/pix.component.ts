2eaec6cfffbd00c8c4726d40c4cd5c991e5a5d8f
Lorena Andrade <lorenaandrade@gmail.com>
2024-05-27 11:09:12 -0300

    components para home

--- pagamentospix/src/app/pages/app/pix/pix.component.ts
+++ pagamentospix/src/app/pages/app/pix/pix.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
-
+import { HeaderComponent } from '../header/header.component';
+import { SidenavComponent } from '../sidenav/sidenav.component';
+import { HomeComponent } from '../home/home.component';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-pix',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, HeaderComponent, SidenavComponent, HomeComponent, RouterOutlet
+  ],
   templateUrl: './pix.component.html',
   styleUrl: './pix.component.css'
 })
