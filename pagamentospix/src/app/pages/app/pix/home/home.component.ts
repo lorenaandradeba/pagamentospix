55126d7b0fb8ef788adf12484bf1917befa949ca
Lorena Andrade <lorenaandrade@gmail.com>
2024-05-27 22:35:32 -0300

    home

--- pagamentospix/src/app/pages/app/pix/home/home.component.ts
+++ pagamentospix/src/app/pages/app/pix/home/home.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input, input } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
-
+export class HomeComponent  {
+  nome: string;
+  src: string;
+  saldo: string;
+  barra: string;
+  constructor() { 
+    this.nome = 'Fulano';
+    this.src = "/assets/icons/fulano.svg";
+    this.barra = "/assets/icons/progresso.svg";
+    this.saldo = '100,00';
+  }
 }
