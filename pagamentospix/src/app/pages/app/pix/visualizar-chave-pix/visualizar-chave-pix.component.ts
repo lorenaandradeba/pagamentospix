8f6319ac20bfd796ec925de6a601d16db87ae5de
Daniel Oliveira da Silva <danieloliveira.s558@gmail.com>
2024-05-28 23:01:14 -0300

    adicionando

--- pagamentospix/src/app/pages/app/pix/visualizar-chave-pix/visualizar-chave-pix.component.ts
+++ pagamentospix/src/app/pages/app/pix/visualizar-chave-pix/visualizar-chave-pix.component.ts
@@ -1,12 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-visualizar-chave-pix',
-  standalone: true,
-  imports: [],
   templateUrl: './visualizar-chave-pix.component.html',
-  styleUrl: './visualizar-chave-pix.component.css'
+  standalone: true,
+  imports: [CommonModule],
+  styleUrls: ['./visualizar-chave-pix.component.css']
 })
 export class VisualizarChavePixComponent {
+  pixKeys: { type: string, key: string }[] = [];
 
+  constructor() {
+    // Criando um array de chaves Pix
+    this.pixKeys = [
+      { type: 'CPF', key: '123.456.789-00' },
+      { type: 'Email', key: 'exemplo@dominio.com' },
+      { type: 'Telefone', key: '+5511999999999' },
+      { type: 'Aleatória', key: 'a1b2c3d4e5f6g7h8i9j0k1l2m3n4o5p6' }
+    ];
+  }
 }
