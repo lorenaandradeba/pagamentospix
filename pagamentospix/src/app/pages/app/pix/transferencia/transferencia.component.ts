49597f043b2ea4075b593541ae6f7aa449229645
Ana Caroline <carolnerisfonseca@gmail.com>
2024-05-28 22:32:45 -0300

    transferencia pronto

--- pagamentospix/src/app/pages/app/pix/transferencia/transferencia.component.ts
+++ pagamentospix/src/app/pages/app/pix/transferencia/transferencia.component.ts
@@ -1,12 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-transferencia',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule ],
   templateUrl: './transferencia.component.html',
   styleUrl: './transferencia.component.css'
 })
 export class TransferenciaComponent {
 
+  transferenciaForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.transferenciaForm = this.fb.group({
+      keyType: ['']
+    });
+  }
+  onSubmit() {
+    console.log('Selected key type:', );}
+
+    formGroup() {
+      console.log('Selected key type:', );
+    }
 }
