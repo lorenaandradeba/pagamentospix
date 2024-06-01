7fb038f3bc37c11bbd57027255edca3513389820
Lorena Andrade <lorenaandrade@gmail.com>
2024-06-01 15:35:54 -0300

    carrega saldo pix

--- pagamentospix/src/app/pages/app/pix/transferencia/transferencia.component.ts
+++ pagamentospix/src/app/pages/app/pix/transferencia/transferencia.component.ts
@@ -1,7 +1,10 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, RequiredValidator, Validators } from '@angular/forms';
 import { AppMaskDirective } from '../../../../pipi/app-mask.directive';
+import { ContaPessoa } from '../../../../models/ContaPessoa';
+import { AuthenticationService } from '../../../../services/auth/authentication.service';
+import { BancoService } from '../../../../services/banco.service';
 
 @Component({
   selector: 'app-transferencia',
@@ -10,23 +13,53 @@ import { AppMaskDirective } from '../../../../pipi/app-mask.directive';
   templateUrl: './transferencia.component.html',
   styleUrl: './transferencia.component.css'
 })
-export class TransferenciaComponent {
+export class TransferenciaComponent  implements OnInit {
 
   transferenciaForm: FormGroup;
-  //empresção lambida
+  saldo: string = '';
+  idUsuario: string ='';
   Banco = {
-    Insituicao: "Banco do Brasil S.A.",
-    Agencia: "19-1",
-    Conta: "10.9956-9",
-    Nome: "Daniel Oliveira da Silva"
+    Insituicao: '',
+    Agencia: '',
+    Conta: '',
+    Nome: ''
   };
+  
+  contaPessoa: ContaPessoa = {
+    IdUsuario: '',
+    IdContaPessoa: '',
+    Nome: '',
+    CPF: '',
+    email: '',
+    senha: '',
+    telefone: '',
+    Numero: 0,
+    Agencia: 0,
+    Saldo: 0
+};
 
-  constructor(private fb: FormBuilder) {
+
+  constructor(private fb: FormBuilder,
+    private authService: AuthenticationService,
+    private bancoService: BancoService
+  ) {
     this.transferenciaForm = this.fb.group({
       valorTransferencia: ['', Validators.required],
       chavePix: ['', Validators.required],
 
     });
+  }
+  ngOnInit(): void {
+    this.idUsuario = this.authService.getUsuarioAutenticado();
+    if (this.idUsuario !== '') {
+      this.bancoService.getContaPessoa(this.idUsuario).subscribe((resposta: any) => {
+        if (resposta) {
+          const chave = Object.keys(resposta)[0]; 
+          const pessoaResp = resposta[chave]; 
+          this.saldo = pessoaResp.Saldo != null ? pessoaResp.Saldo.toFixed(2) : '0.00';
+        }
+      });      
+    }
   }
   onSubmit() {
    if (this.transferenciaForm.valid) {
